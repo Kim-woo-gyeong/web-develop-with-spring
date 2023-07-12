8ff8fd45d72ba67d31862d6f3626f389ba295328
김우경 <lovelykwg@naver.com>
2023-07-12 19:53:54 +0900

    객체지향연습04 : @Test import 수정

--- oop-practices04/src/test/java/org/example/CookingTest.java
+++ oop-practices04/src/test/java/org/example/CookingTest.java
@@ -1,8 +1,8 @@
 package org.example;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class CookingTest {
 	@Test
