d4193bb8281b88399667951b9a5814be9ea47c6c
김우경 <lovelykwg@naver.com>
2023-08-18 16:36:34 +0900

    의존성주입하기연습 : beanFactoryTest 수정

---
+++ oop-practices/src/test/java/diPractice/org/example/BeanFactoryTest.java
@@ -0,0 +1,45 @@
+package diPractice.org.example;
+
+import org.assertj.core.api.Assertions;
+import org.example.annotation.Controller;
+import org.example.annotation.Service;
+import org.example.controller.UserController;
+import org.example.di.BeanFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BeanFactoryTest {
+    private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
+    private Reflections reflections;
+    private BeanFactory beanFactory;
+    @BeforeEach
+    void setUp(){
+        reflections = new Reflections("org.example");
+        Set<Class<?>> preInstantiated = getTypesAnnotiatedWith(Controller.class, Service.class);
+        log.info("preInstantiated = {}", preInstantiated);
+        beanFactory = new BeanFactory(preInstantiated);
+    }
+
+    private Set<Class<?>> getTypesAnnotiatedWith(Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = new HashSet<>();
+        for(Class<? extends Annotation> annotation : annotations){
+            // "org.example" 하위 폴더에 annotation 이 붙은 class 를 뽑아냄.
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return beans;
+    }
+
+    @Test
+    void diTest(){
+        log.info("beanFactory = {}",beanFactory);
+        UserController userController = beanFactory.getBean(UserController.class);
+        Assertions.assertThat(userController).isNotNull();
+    }
+}
